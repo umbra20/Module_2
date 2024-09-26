dae3c9fee95f5038cf8d0e8c1b21c787becde8d8
Yura <76448413+umbra20@users.noreply.github.com>
2024-09-26 16:03:32 +0300

    Update SumStroka.java

--- StreamAPI/SumStroka.java
+++ StreamAPI/SumStroka.java
@@ -1,23 +1,26 @@
-package Module_2.StreamAPI;
-//TODO Создайте список типа String, например, названия пищевых
-// продуктов. Напишите программу для подсчета количества строк в
-// списке, начинающихся с определенной буквы, с использованием потоков.
-
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class SumStroka {
     public static void main(String[] args) {
 
         List<String> stroka = Arrays.asList("Молоко", "Хлеб", "Шоколад", "Халва", "Творог");
-        System.out.println("Количество строк, начинающихся с: " + stroka);
+        if (stroka.isEmpty()) {
+            System.out.println("Список массивов пуст.");
+        } else {
+            System.out.println("Количество строк, начинающихся с: " + stroka);
 
-        char startLetter = 'Х';
-        // Подсчет кол-во строк, начинающихся с определенной буквы
-        long str = stroka.stream()
-                .filter(s -> s.startsWith(String.valueOf(startLetter)))
-                .count();
-        System.out.println("\n Количество строк, начинающихся с '"
-                + startLetter + "': " + str);
+            char startLetter = 'Х';
+
+            // Подсчет кол-во строк, начинающихся с определенной буквы
+            long str = stroka.stream()
+                    .filter(s -> s.startsWith(String.valueOf(startLetter)))
+                    .count();
+
+            System.out.println("\n Количество строк, начинающихся с '"
+                    + startLetter + "': " + str);
+
+        }
     }
 }
